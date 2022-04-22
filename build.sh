bd9e95b26f5660513a851411241fb2ee9aa1b023
Lautaro Matas <lmatas@gmail.com>
2022-04-22 17:05:49 +0200

    Solr Configuration fix

--- build.sh
+++ build.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
-#
-# Copyright (c) 2022. LA Referencia / Red CLARA and others
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# This file is part of LA Referencia software platform LRHarvester v4.x
-# For any further information please contact Lautaro Matas <lmatas@gmail.com>
-#
-#
 
-mvn clean package install -DskipTests -Dmaven.javadoc.skip=true
+if [ -z "$1" ]; then
+   mvn clean package install -DskipTests -Dmaven.javadoc.skip=true
+else
+   mvn clean package install -DskipTests -Dmaven.javadoc.skip=true -P$1
+fi
+
--- src/main/java/org/lareferencia/shell/commands/entity/SolrConfiguration.java
+++ src/main/java/org/lareferencia/shell/commands/entity/SolrConfiguration.java
@@ -1,22 +1,23 @@
 
 /*
- *   Copyright (c) 2013-2022. LA Referencia / Red CLARA and others
+ * Copyright (c) 2013-2022. LA Referencia / Red CLARA and others
  *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU Affero General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- *   You should have received a copy of the GNU Affero General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *   This file is part of LA Referencia software platform LRHarvester v4.x
- *   For any further information please contact Lautaro Matas <lmatas@gmail.com>
+ * This file is part of LA Referencia software platform LRHarvester v4.x
+ * For any further information please contact Lautaro Matas <lmatas@gmail.com>
+ *
  */
 package org.lareferencia.shell.commands.entity;
 
@@ -47,15 +48,7 @@ public class SolrConfiguration  {
 	
 	/** Configurations beans for solr services */
     @Bean
-    public SolrClient solrClient(@Value("${solr.host}") String solrHost) {
-        return new HttpSolrClient.Builder(solrHost).build();
-    }
- 
-    @Bean
-    public SolrTemplate solrTemplate(SolrClient client) throws Exception {
+    public SolrTemplate shellSolrTemplate(SolrClient client) throws Exception {
         return new SolrTemplate(client);
-    }	
-    
-   
-    
+    }
  }
